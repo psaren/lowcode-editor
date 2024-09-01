2cff3d72da75ecea8d33446d74fb341d4333cba5
psaren <18718574603@163.com>
2024-09-01 12:19:33 +0800

    feat: 画布区Hover高亮效果

--- src/editor/components/EditArea.tsx
+++ src/editor/components/EditArea.tsx
@@ -1,29 +1,31 @@
-import React, { useEffect } from "react";
+import React, { MouseEventHandler, useState } from "react";
 import { useComponentConfigStore } from "../stores/component-config";
 import { Component, useComponetsStore } from "../stores/components";
+import HoverMask from "./HoverMask";
 
 export function EditArea() {
-  const { components, addComponent } = useComponetsStore();
+  const { components } = useComponetsStore();
   const { componentConfig } = useComponentConfigStore();
 
-  useEffect(() => {
-    // addComponent({
-    //   id: 222,
-    //   name: 'Container',
-    //   props: {},
-    //   children: []
-    // }, 1);
+  const [hoverComponentId, setHoverComponentId] = useState<number>();
 
-    // addComponent({
-    //   id: 333,
-    //   name: 'Button',
-    //   props: {
-    //     text: '无敌'
-    //   },
-    //   children: []
-    // }, 222);
-  }, []);
-  console.log('components :>> ', components);
+  const handleMouseOver: MouseEventHandler = (e) => {
+    const path = e.nativeEvent.composedPath();
+
+    for (let i = 0; i < path.length; i += 1) {
+      const ele = path[i] as HTMLElement;
+
+      const componentId = ele.dataset?.componentId;
+      if (componentId) {
+        setHoverComponentId(+componentId);
+        return;
+      }
+    }
+  }
+  const handleMouseLeave = () => {
+    setHoverComponentId(undefined);
+  }
+
 
   function renderComponents(components: Component[]): React.ReactNode {
     return components.map((component: Component) => {
@@ -46,10 +48,15 @@ export function EditArea() {
     })
   }
 
-  return <div className="h-[100%]">
-    {/* <pre>
-      {JSON.stringify(components, null, 2)}
-    </pre> */}
+  return <div className="h-[100%] edit-area" onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
     {renderComponents(components)}
+    {hoverComponentId && (
+      <HoverMask
+        portalWrapperClassName='portal-wrapper'
+        containerClassName='edit-area'
+        componentId={hoverComponentId}
+      />
+    )}
+    <div className="portal-wrapper"></div>
   </div>
 }
